cc703dc9d3925ae3b5f8fd9a1a8cc965df674600
Felipe Pericles Sousa <felipe.pericles.sousa@gmail.com>
2026-02-27 14:27:27 -0300

    Finalização do projeto
    Projeto completo do Jogo da velha em C.
    Projeto de lógica de programação

--- main.c
+++ main.c
@@ -1,40 +1,155 @@
 #include <stdio.h>
 
+char matriz[3][3];
+int opcao;
+int linha, coluna;
+char jogador = 'X';  
+
+void inicializar_tabuleiro () {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      matriz[i][j] = ' ';
+    }
+  }
+}
+
+void exibir_tabuleiro () {
+  printf("    0    1    2 \n");
+  for (int i = 0; i < 3; i++) {
+    printf("%d   ", i);
+    for (int j = 0; j < 3; j++) {
+      if (j < 2) {
+        printf(" %c |", matriz[i][j]);
+      } else {
+        printf(" %c ", matriz[i][j]);
+      }
+    }
+    if (i < 2) {
+      printf("\n ----------------\n");
+    }
+  }
+  printf("\n");
+}
+
+int verificar_vencedor() {
+  for (int i = 0; i < 3; i++) {
+    if (matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2] && matriz[i][0] != ' ')
+      return 1;
+  }
+  for (int j = 0; j < 3; j++) {
+    if (matriz[0][j] == matriz[1][j] && matriz[1][j] == matriz[2][j] && matriz[0][j] != ' ')
+      return 1;
+  }
+  if (matriz[0][0] == matriz[1][1] && matriz[1][1] == matriz[2][2] && matriz[0][0] != ' ')
+    return 1;
+  if (matriz[0][2] == matriz[1][1] && matriz[1][1] == matriz[2][0] && matriz[0][2] != ' ')
+    return 1;
+  return 0;
+}
+
+void AlternarJogador (){
+  if (jogador == 'X') {
+    jogador = 'O';
+  } else {
+    jogador = 'X';
+  }
+}
+
+void FucaoJogada(){
+  printf("\nVez do jogador %c\n", jogador);
+
+  do {
+    printf("Digite a linha (0-2): ");
+    scanf("%d", &linha);
+    printf("Digite a coluna (0-2): ");
+    scanf("%d", &coluna);
+
+    if (linha < 0 || linha > 2 || coluna < 0 || coluna > 2) {
+      printf("Coordenadas inválidas! Tente novamente.\n");
+      continue;
+    }
+
+    if (matriz[linha][coluna] != ' ') {
+      printf("Essa posição já está ocupada! Escolha outra.\n");
+    }
+
+  } while (linha < 0 || linha > 2 || coluna < 0 || coluna > 2 || matriz[linha][coluna] != ' ');
+
+  matriz[linha][coluna] = jogador;
+}
+
+int Empate() {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (matriz[i][j] == ' ') {
+        return 0;
+      }
+    }
+  }
+
+  if (!verificar_vencedor()) {
+    return 1; 
+  }
+  return 0; 
+}
+
 int main(void) {
-  char nome1[100];
-  char nome2[100];
-  int opcao;
-  printf("Seja bem vindo ao jogo da velha do EducaHub.\n");
+
+  printf("Seja bem-vindo ao jogo da velha do EducaHub.\n");
   printf("Aqui você aprende jogando!\n");
   printf("Vamos começar? Escolha uma opção do menu abaixo\n");
   printf("1 - Jogar\n");
-  printf("2 - Intruções\n");
+  printf("2 - Instruções\n");
   printf("3 - Sair\n");
   printf("Digite sua opção: ");
-  scanf ("%d", &opcao);
+  scanf("%d", &opcao);
+
   switch (opcao) {
     case 1:
-      printf("Excelente que você escolheui jogar. Antes de tudo vamos personalizar o jogo com os nomes dos jogadores!\n");
-      printf("Digite o nome do jogador 1 (X):\n");
-      fgets(nome1, 100, stdin); 
-      printf("Digite o nome do jogador 2 (O):\n");
-      fgets(nome2, 100, stdin);  
-      printf ("Ahh que pena!! O jogo está indisponível, volte mais tarde!\n");
+      printf("Excelente que você escolheu jogar.\n");
+      printf("Jogador 1 será o X.\n");
+      printf("Jogador 2 será o O.\n");
+      printf("Observe o tabuleiro!!\n");
+
+      inicializar_tabuleiro ();
+      exibir_tabuleiro ();
+
+      for (int jogada = 0; jogada < 9; jogada++) {
+        FucaoJogada ();
+        exibir_tabuleiro ();
+
+        if (verificar_vencedor()) {
+          printf("Jogador %c venceu! Parabéns!\n", jogador);
+          break;
+        }
+
+        if (Empate()) {
+          printf("Empate! Ninguém venceu.\n");
+          printf("Recomendo jogar novamente!.\n");
+          break;
+        }
+
+        AlternarJogador ();
+      }
+
       break;
+
     case 2:
-      printf ("Para começar, deve-se olhar para o tabuleiro, com as 9 casas.\n");
-      printf ("Em seguida, deve-se escolher entre jogar o jogador um (X) e o jogador dois (O). \n");
-      printf ("Jogando um de cada vez. O objetivo do jogo é fazer uma fila de três de seu símbolo escolhido, seja verticalmente, horizontalmente, ou diagonalmente. \n");
-      printf ("Porém, a segunda pessoa com quem está jogando tem o mesmo objetivo, e os dois jogadores tem que tentar ser mais espertos do que o outro para fazer a linha de três símbolos.\n");
-      printf ("Parece divertido né ? Então, vamos jogar?\n");
+      printf("Para começar, deve-se olhar para o tabuleiro, com as 9 casas.\n");
+      printf("Em seguida, deve-se escolher entre o jogador um (X) e o jogador dois (O).\n");
+      printf("Jogando um de cada vez. O objetivo é fazer uma fila de três símbolos iguais — horizontal, vertical ou diagonal.\n");
+      printf("Divirta-se!\n");
       break;
+
     case 3:
-      printf ("Sentimos por ver você partir!!\n");
-      printf ("Tente nos visitar sempre que possível! Esperaremos por você!! \n");
+      printf("Sentimos por ver você partir!!\n");
+      printf("Tente nos visitar sempre que possível! Esperaremos por você!\n");
       break;
+
     default:
-      printf ("Opção inválida! Tente digitar outro número!! \n");
+      printf("Opção inválida! Tente digitar outro número!\n");
       break;
   }
+
   return 0;
-}
+  }
